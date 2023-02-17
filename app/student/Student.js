1cad4514c6e47cd2938243f6ecc4a27faf8eff49
Manav Misra <manavm@visionwebsoft.com>
2023-02-16 20:42:51 -0600

    Add student schema

---
+++ app/student/Student.js
@@ -0,0 +1,22 @@
+import { Schema, model } from "mongoose";
+import gradeSchema from "./grade-schema.js";
+
+const studentSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Student name is required"],
+      minLength: [3, "Student name must be at least 3 characters long"],
+      trim: true,
+      // TODO: Add custom validator to ensure only letters and one space in between words
+    },
+
+    // An array of subdocuments.
+    grades: [gradeSchema],
+    // TODO: Add a virtual property to calculate the student's average grade
+  },
+  {
+    strict: "throw",
+    versionKey: false,
+  }
+);
