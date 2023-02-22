495eedd27fa38c684fc734df8130ad7dad3d4aad
Manav Misra <manavm@visionwebsoft.com>
2023-02-21 22:21:22 -0600

    :poop: 18-3

--- app/student/controller.js
+++ app/student/controller.js
@@ -1,8 +1,9 @@
+import initDB from "../client.js";
 import Student from "./Student.js";
 
 const controller = {
   index() {
-    return Student.find();
+    Student.find();
   },
   showStudent(studentId) {
     return Student.findById(studentId);
@@ -17,13 +18,48 @@ const controller = {
     return Student.findByIdAndUpdate(studentId, { name: newName4Student });
   },
   updateStudentWithNewGrade(studentId, newGrade) {
-    return Student.findByIdAndUpdate(studentId, {
-      $push: { grades: newGrade },
-    });
+    return Student.findByIdAndUpdate(
+      studentId,
+      {
+        $push: { grades: newGrade },
+      },
+      {
+        // 'new' returns the updated document
+        new: true,
+        runValidators: true,
+      }
+    );
   },
   updateStudentGrade(id, gradeId, newEarnedGrade) {
     // TODO: Implement this method.
   },
 };
+
+await initDB();
+
+// controller
+//   .create({
+//     fullName: "John Doe",
+//     github: "johndoe",
+//   })
+//   .then((student) => {
+//     console.info(student);
+//   })
+//   .catch((error) => {
+//     console.error(error.message);
+//   });
+
+controller
+  .updateStudentWithNewGrade("63f535b7f97b3c802df2fd74", {
+    name: "Exam 1",
+    earned: 100,
+    possible: 100,
+  })
+  .then((student) => {
+    console.info(student);
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
 
 export default controller;
