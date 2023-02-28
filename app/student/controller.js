2f90e707759c78abb18781b4a19fefb373e4e23a
Manav Misra <manavm@visionwebsoft.com>
2023-02-27 18:10:35 -0600

    Remove controller manual test code

--- app/student/controller.js
+++ app/student/controller.js
@@ -1,4 +1,3 @@
-import initDB from "../client.js";
 import Student from "./Student.js";
 
 const controller = {
@@ -34,32 +33,5 @@ const controller = {
     // TODO: Implement this method.
   },
 };
-
-await initDB();
-
-// controller
-//   .create({
-//     fullName: "John Doe",
-//     github: "johndoe",
-//   })
-//   .then((student) => {
-//     console.info(student);
-//   })
-//   .catch((error) => {
-//     console.error(error.message);
-//   });
-
-controller
-  .updateStudentWithNewGrade("63f535b7f97b3c802df2fd74", {
-    name: "Exam 1",
-    earned: 100,
-    possible: 100,
-  })
-  .then((student) => {
-    console.info(student);
-  })
-  .catch((err) => {
-    console.error(err.message);
-  });
 
 export default controller;
